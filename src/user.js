5ddfbc63e8aafa5b8a64822189195ae260e19d69
Tejush <tejush@aegiscovenant.com>
2023-03-03 14:47:18 +0530

    user

--- src/user.js
+++ src/user.js
@@ -1,5 +1,12 @@
 const Router = require("express")
 const user = Router();
+require('dotenv').config()
+const neo4j = require('neo4j-driver')
+const { url, username, password, database } = process.env;
+const driver = neo4j.driver(url, neo4j.auth.basic(username, password), {
+  /* encrypted: 'ENCRYPTION_OFF' */
+});
+const session = driver.session({ database });
 
 user.get("/", async (req, res) => {
     const result = await session.run("Match (u:User) return u");
