2928563f61116a25efa2c74b94d17d11eb391b71
Tejush <tejush@aegiscovenant.com>
2023-03-03 14:26:21 +0530

    whitebox

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 require('dotenv').config()
-
+const neo4j = require('neo4j-driver')
 const app = express();
+const { url, username, password, database } = process.env;
+const driver = neo4j.driver(url, neo4j.auth.basic(username, password), {
+  /* encrypted: 'ENCRYPTION_OFF' */
+});
+
+const session = driver.session({ database });
+
 
 app.get("/", (req, res) => {
-  res.send(process.env.url);
+  res.send(url,username);
 });
 
 app.listen(5000, () => {
