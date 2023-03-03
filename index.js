d961ffedda22315f4f12001bb2fd7d2cd3b09aa4
Tejush <tejush@aegiscovenant.com>
2023-03-03 14:05:17 +0530

    url

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+require('dotenv').config()
+const neo4j = require('neo4j-driver')
+
+const { url, username, password, database } = process.env;
+const driver = neo4j.driver(url, neo4j.auth.basic(username, password), {
+  /* encrypted: 'ENCRYPTION_OFF' */
+});
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send(url);
 });
 
 app.listen(5000, () => {
