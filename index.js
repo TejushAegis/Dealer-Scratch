aa388897e38b9cf4656518c91c164317d0a7226c
Tejush <tejush@aegiscovenant.com>
2023-03-03 13:43:34 +0530

    Revert "minor"
    This reverts commit 887ecbfb02e813acca2be08237464ee84c8538de.

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 // Add Express
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import neo4j from "neo4j-driver";
 
-const { url, username, password, database } = process.env;
-
-const driver = neo4j.driver(url, neo4j.auth.basic(username, password), {
-    /* encrypted: 'ENCRYPTION_OFF' */
-  });
-  
-  const session = driver.session({ database });
-  
 // Initialize Express
 const app = express();
 
 // Create GET request
-
-app.get("/", async (req, res) => {
-    const result = await session.run("Match (u:User) return u");
-  let results =  result.records.map((i) => i.get("u").properties);
-  res.send(url);
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
 // Initialize server
